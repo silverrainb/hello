680fd970280d42f6bd189d4c425d8bc4a63f6098
silverrainb <git-ebk@opayq.com>
2020-01-26 18:11:47 -0600

    adding all files

---
+++ app.js
@@ -0,0 +1,14 @@
+function randomRGB(){
+    const r = Math.floor(Math.random() * 256)
+    const g = 0
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+const letters = document.querySelectorAll('.letter')
+
+setInterval(function () {
+    for(let letter of letters){
+        letter.style.color = randomRGB()
+    }
+})
